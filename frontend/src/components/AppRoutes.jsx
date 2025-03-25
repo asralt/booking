16ed8a0e29bd079ddcf46d482711232269d840a9
aska <enkhbadralasral00@gmail.com>
2025-03-25 14:03:06 +0000

    added Error Page e

--- frontend/src/components/AppRoutes.jsx
+++ frontend/src/components/AppRoutes.jsx
@@ -1,15 +1,24 @@
 import {useRoutes} from "react-router-dom"
 import HomePage from "../pages/HomePage"
+// import ErrorBoundaries from "./ErrorBoundaries";
+import ErrorPage from "../pages/404";
 
 
 export const AppRoutes =()=> {
     const  routes = useRoutes([
-        {path:'/' , element: <HomePage/>}
+        {path:'/' , element: <HomePage/>},
+        {path: "*", element: <ErrorPage/>}
 
     ]);
 
-    return routes;
+    return(
+        // <ErrorBoundaries>
+            routes
+        // </ErrorBoundaries>
+    )
+
 };
+
 
 
 export default AppRoutes
---
+++ frontend/src/pages/404.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div>
+        this will be 404
+    </div>
+  )
+}
+
+export default ErrorPage
